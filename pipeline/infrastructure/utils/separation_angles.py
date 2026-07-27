9e034efc6ef0e34b85cc0fe688b35f47c65d3d19
Rui Xue <rxue@nrao.edu>
2026-07-27 10:14:39 -0400

    Applied suggestion

--- pipeline/infrastructure/utils/separation_angles.py
+++ pipeline/infrastructure/utils/separation_angles.py
@@ -3,7 +3,6 @@
 # Do not evaluate type annotations at definition time.
 from __future__ import annotations
 
-import collections
 import itertools
 from typing import TYPE_CHECKING
 import numpy as np
