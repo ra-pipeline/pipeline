5256868e884ca0381300f2b8886c2597308f9951
Rui Xue <rx.astro@gmail.com>
2026-08-19 23:00:41 -0500

    PIPE-3219: Remove `findroi_products.tar` from weblog rendering
    Skip copying large `findroi_products.tar` to weblog directory and remove from artifact links. The tarball remains in findroi_workdir and is still included in auxiliary products export.

--- pipeline/hif/tasks/findroi/renderer.py
+++ pipeline/hif/tasks/findroi/renderer.py
@@ -42,7 +42,6 @@ class T2_4MDetailsFindROIRenderer(basetemplates.T2_4MDetailsDefaultRenderer):
         artifact_links = []
         for label, key in (
             ('Full stage product pickle', 'results_pickle'),
-            ('FindROI products tar', 'findroi_products_tar'),
             ('ROI.dat', 'roi_dat'),
             ('ROIcont.dat', 'roi_cont_dat'),
         ):
