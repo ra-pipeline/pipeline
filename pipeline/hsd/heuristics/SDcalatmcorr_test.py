5334b87ca06d01b4a99a854d5d44e06596390e6d
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2026-08-27 08:51:03 +0900

    fix wrong index in docstring

--- pipeline/hsd/heuristics/SDcalatmcorr_test.py
+++ pipeline/hsd/heuristics/SDcalatmcorr_test.py
@@ -105,9 +105,9 @@ def test_getTau_partial():
     is shown below. "=" indicates selected channels.
 
     freq (LSB): low                                         high
-      spw chan: 100        0
+      spw chan:  99        0
                  |=========|---------------------------------|
-      tau chan: 128           100                            0
+      tau chan: 127            99                            0
                  |-------------|=============================|
 
     If the bug emerges, resulting tau will be negative.
